3a240b6332258ee4064fc2874c2382d5d06a072d
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-06 00:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy Holidays, ${name}`
 }
 
-function happyHolidaysTo ()
+function happyHolidaysTo (holiday,name) {
+  return `Happy ${holiday}, ${name}`
+}
+
+function holidayCountdown(days,holiday) {
+  return `It's ${days} days until ${holiday}`
+}
